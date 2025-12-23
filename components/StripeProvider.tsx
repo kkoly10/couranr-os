94d14319ed1eb4e8ee55633608371ad55b1b976d
kkoly10 <komlankouhiko@icloud.com>
2025-12-23 11:55:56 -0500

    Update StripeProvider.tsx

--- components/StripeProvider.tsx
+++ components/StripeProvider.tsx
@@ -1,27 +1,13 @@
 "use client";
 
 import { ReactNode } from "react";
-import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 );
 
 export default function StripeProvider({ children }: { children: ReactNode }) {
-  return (
-    <Elements
-      stripe={stripePromise}
-      options={{
-        appearance: {
-          theme: "stripe",
-          variables: {
-            colorPrimary: "#2563eb"
-          }
-        }
-      }}
-    >
-      {children}
-    </Elements>
-  );
+  return <Elements stripe={stripePromise}>{children}</Elements>;
 }
