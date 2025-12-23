1c861013af0e968e04a0cdb34ad0211c9d426b18
kkoly10 <komlankouhiko@icloud.com>
2025-12-23 12:22:49 -0500

    Update page.tsx

--- app/courier/checkout/page.tsx
+++ app/courier/checkout/page.tsx
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
 import AuthGuard from "../../../components/AuthGuard";
-import StripeProvider from "../../../components/StripeProvider";
 
 const CheckoutClient = dynamic(
   () => import("../../../components/courier/CheckoutClient"),
@@ -10,16 +9,7 @@ const CheckoutClient = dynamic(
 export default function CourierCheckoutPage() {
   return (
     <AuthGuard>
-      <CheckoutClientWrapper />
-    </AuthGuard>
-  );
-}
-
-function CheckoutClientWrapper() {
-  // StripeProvider will be activated once clientSecret is set
-  return (
-    <StripeProvider clientSecret={null}>
       <CheckoutClient />
-    </StripeProvider>
+    </AuthGuard>
   );
 }
