e7f4e91316e63eb091d8ff2afa29c929d3f324c0
kkoly10 <komlankouhiko@icloud.com>
2025-12-24 23:57:50 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,19 +7,13 @@ export const metadata = {
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "var(--bg)"
-        }}
-      >
+      <body>
         <Header />
         {children}
       </body>
