8f419c10a46386cc3466eac18c1b1aca43c19b77
kkoly10 <komlankouhiko@icloud.com>
2025-12-24 23:47:36 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          background: "var(--bg)"
+        }}
+      >
         <Header />
         {children}
       </body>
