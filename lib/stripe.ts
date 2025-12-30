a9dbbd0e8466616fa737e8954bcc737a2f675933
kkoly10 <komlankouhiko@icloud.com>
2025-12-30 11:31:59 -0500

    Update Stripe API version to 2024-04-10

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-
-  apiVersion: "2024-06-20"
-
-});
- 
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY as string,
+  {
+    apiVersion: "2024-04-10", // ✅ MUST match installed SDK
+  }
+);
