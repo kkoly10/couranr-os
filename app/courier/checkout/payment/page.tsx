0770294cf3d16348ae2ff419b5d38a1e4f7e7737
kkoly10 <komlankouhiko@icloud.com>
2025-12-30 08:46:00 -0500

    Update page.tsx

--- app/courier/checkout/payment/page.tsx
+++ app/courier/checkout/payment/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
