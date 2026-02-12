21980d273655415f0f3bfebacde732398c0bb421
kkoly10 <komlankouhiko@icloud.com>
2026-02-12 10:55:01 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,8 +3,4 @@
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-/**
- * Client-side Supabase (cookie-synced).
- * Use this everywhere in client components.
- */
 export const supabase = createClientComponentClient();
