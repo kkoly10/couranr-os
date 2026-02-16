a87325c75a3c0913bb74393f3a7e48e971aceadd
kkoly10 <komlankouhiko@icloud.com>
2026-02-16 13:18:25 -0500

    Update Brand.tsx

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 export default function Brand({
-  href,
+  href = "/",
   role,
 }: {
-  href: string;
+  href?: string;
   role?: "admin" | "driver" | "customer" | string;
 }) {
   const badge =
