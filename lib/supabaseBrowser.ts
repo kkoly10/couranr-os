1dce92c5312b17f672ee0ee37e8304755bcfe0ee
kkoly10 <komlankouhiko@icloud.com>
2026-02-16 13:22:11 -0500

    Update supabaseBrowser.ts

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -1,5 +1,10 @@
 "use client";
 
+import { // lib/supabaseBrowser.ts
+"use client";
+
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export const supabaseBrowser = createClientComponentClient(); } from "@supabase/auth-helpers-nextjs";
 
 export const supabaseBrowser = createClientComponentClient();
