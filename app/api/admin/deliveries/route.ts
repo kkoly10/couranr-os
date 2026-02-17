b689adbc45ac8b4590e1ba7a82ea0d56d0dfbdaa
kkoly10 <komlankouhiko@icloud.com>
2026-02-17 07:32:33 -0500

    Update route.ts

--- app/api/admin/deliveries/route.ts
+++ app/api/admin/deliveries/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const supabase = createRouteHandlerClient({ cookies });
 
